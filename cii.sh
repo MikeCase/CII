e09e916aae9a9cc83b8babb99d1e6434ee5975cc
Michael Case <michael.e.case@gmail.com>
2011-09-29 09:26:00 -0500

    Added the ability to install sparks durring CI installation

--- cii.sh
+++ cii.sh
@@ -8,10 +8,10 @@ do
 					clear
 					echo "What is the name of your app? "
 					read APPNAME
-					`wget http://downloads.codeigniter.com/reactor/CodeIgniter_2.0.3.zip`
-					`unzip CodeIgniter_2.0.3.zip -d ${APPNAME}`
-					`mv ${APPNAME}/CodeIgniter_2.0.3/* ${APPNAME}`
-					`rm -rf ${APPNAME}/CodeIgniter_2.0.3/`
+					wget http://downloads.codeigniter.com/reactor/CodeIgniter_2.0.3.zip
+					unzip CodeIgniter_2.0.3.zip -d ${APPNAME}
+					mv ${APPNAME}/CodeIgniter_2.0.3/* ${APPNAME}
+					rm -rf ${APPNAME}/CodeIgniter_2.0.3/
 					exit
 					;;
 		"install version")
@@ -20,10 +20,10 @@ do
 					read APPNAME
 					echo "What version do you want to install? " 
 					read VERSION
-					`wget http://downloads.codeigniter.com/reactor/CodeIgniter_${VERSION}.zip`
-					`unzip CodeIgniter_${VERSION}.zip -d ${APPNAME}`
-					`mv ${APPNAME}/CodeIgniter_${VERSION}/* ${APPNAME}`
-					`rm -rf ${APPNAME}/CodeIgniter_${VERSION}/`
+					wget http://downloads.codeigniter.com/reactor/CodeIgniter_${VERSION}.zip
+					unzip CodeIgniter_${VERSION}.zip -d ${APPNAME}
+					mv ${APPNAME}/CodeIgniter_${VERSION}/* ${APPNAME}
+					rm -rf ${APPNAME}/CodeIgniter_${VERSION}/
 					exit
 					;;
 		"install w/sparks")
@@ -32,11 +32,35 @@ do
 					read APPNAME
 					echo "What version do you want to install? "
 					read VERSION
-					`wget http://downloads.codeigniter.com/reactor/CodeIgniter_${VERSION}.zip`
-					`unzip CodeIgniter_${VERSION}.zip -d ${APPNAME}`
-					`mv ${APPNAME}/CodeIgniter_${VERSION}/* ${APPNAME}`
-					`rm -rf ${APPNAME}/CodeIgniter_${VERSION}/`
-					`cd ${APPNAME}/ && php -r "$(curl -fsSL http://getsparks.org/go-sparks)"`
+					echo "Would you like to install sparks now?"
+					read YESNO
+					if [ $YESNO == "yes" ]; then
+						declare -a SPARKS
+						echo "What sparks would you like to install (space seperated list)"
+						read -a SPARKS
+						wget http://downloads.codeigniter.com/reactor/CodeIgniter_${VERSION}.zip
+						unzip CodeIgniter_${VERSION}.zip -d ${APPNAME}
+						mv ${APPNAME}/CodeIgniter_${VERSION}/* ${APPNAME}
+						rm -rf ${APPNAME}/CodeIgniter_${VERSION}/
+						cd ${APPNAME}/ && php -r "$(curl -fsSL http://getsparks.org/go-sparks)"
+						for SPARK in "${SPARKS[@]}"
+						do
+							php tools/spark install ${SPARK}
+						done
+
+					elif [ $YESNO == "no" ]; then
+						wget http://downloads.codeigniter.com/reactor/CodeIgniter_${VERSION}.zip
+						unzip CodeIgniter_${VERSION}.zip -d ${APPNAME}
+						mv ${APPNAME}/CodeIgniter_${VERSION}/* ${APPNAME}
+						rm -rf ${APPNAME}/CodeIgniter_${VERSION}/
+						cd ${APPNAME}/ && php -r "$(curl -fsSL http://getsparks.org/go-sparks)"
+					else
+						echo "That is an invalid response!"
+						
+					fi
+
+					
+					
 					exit
 					;;
 		"quit")
